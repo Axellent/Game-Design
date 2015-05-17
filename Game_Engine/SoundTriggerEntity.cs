5690c066fb3a67b6a6f11079d19c941ff4881851
Axellent <axel.sigl@gmail.com>
2015-05-17 20:29:55 +0200

    Added SoundTriggerEntity and tile content

---
+++ Game_Engine/SoundTriggerEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Game_Engine{
+
+	public class SoundTriggerEntity : TriggerEntity{
+
+		public SoundTriggerEntity(string id, float x, float y, float width, float height, float rotation,
+			BoundingBox hitbox) : base(id, x, y, width, height, rotation, hitbox){
+
+		}
+	}
+}
+
