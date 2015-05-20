1d9ac9e7dfe0c882fda638c55bf81b6b5992836d
Axellent <axel.sigl@gmail.com>
2015-05-20 14:19:08 +0200

    Added missing tile.cs

---
+++ Survival_Game/EntityObjects/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Game_Engine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Survival_Game{
+
+	/* Author: Axel Sigl */
+	public class Tile : StaticEntity{
+
+		public Tile(string id, float x, float y, float width, float height, float rotation, BoundingBox hitbox, 
+			int layer, Texture2D texture) : base(id, x, y, width, height, rotation, hitbox, layer, texture){
+		}
+
+		public Tile(string id, float x, float y, float width, float height, float rotation, BoundingBox hitbox, 
+			int layer, Texture2D texture, bool hasCollision)
+			: base(id, x, y, width, height, rotation, hitbox, layer, texture, hasCollision){
+		}
+	}
+}
+
