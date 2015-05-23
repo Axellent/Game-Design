b367e938c5d763bb6c4613c15780054d9f661a02
mawija <martin.wil.hem@hotmail.com>
2015-05-23 15:22:56 +0200

    Removed some outcommented code

--- Survival_Game/EntityObserver.cs
+++ Survival_Game/EntityObserver.cs
@@ -87,8 +87,6 @@ namespace Survival_Game
 								break;
 							}
 
-
-
 							foreach (Tuple <Vector3,Viewport, Entity> pair in engine.Viewposes){
 								if(pair.Item3.ID.Equals(player.ID)){
 									engine.Viewposes.Add(new Tuple<Vector3, Viewport, Entity>(new Vector3(player.X- pair.Item2.Width/2, player.Y - pair.Item2.Height/2, 0),pair.Item2, pair.Item3));
@@ -96,16 +94,7 @@ namespace Survival_Game
 									break;
 								}
 							}
-								//savedEntities.AddRange (entities.FindAll (e => !limitbox.Intersects (e.HitBox)));
-								//entities.RemoveAll (e => !limitbox.Intersects (e.HitBox));
 
-
-									//if(pair.value.name == deafultview) // then do this shit
-									//pair = engine.Viewposes.Find();
-									//engine.Viewposes.Remove(pair);
-									//engine.Viewposes.Add(new Tuple<Vector3, Viewport>(new Vector3(player.X,player.Y,0),pair.Item2));
-														
-					
 							actionMade++;
 						}
 					} else {
