300c40dcf6b2c04b23e4d8fa7731b9ac738d7770
Axellent <axel.sigl@gmail.com>
2015-05-25 03:22:48 +0200

    added Bush.cs

---
+++ Survival_Game/EntityObjects/Bush.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Game_Engine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Survival_Game{
+
+	/* Author: Axel Sigl */
+	public class Bush : StaticEntity{
+
+		public Bush(string id, float x, float y, float width, float height, float rotation, BoundingBox hitbox, 
+			int layer, Texture2D texture) : base(id, x, y, width, height, rotation, hitbox, layer, texture){
+		}
+
+		public Bush(string id, float x, float y, float width, float height, float rotation, BoundingBox hitbox, 
+			int layer, Texture2D texture, bool hasCollision)
+			: base(id, x, y, width, height, rotation, hitbox, layer, texture, hasCollision){
+		}
+	}
+}
