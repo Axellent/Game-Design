401a1c7427f8c960293dfc19ada11f45a4611dcd
unknown <andreas.lonnermark@gmail.com>
2015-05-29 22:31:24 +0200

    Added some functionality for optionmenu

---
+++ Survival_Game/EntityObjects/OptionBar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Game_Engine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Survival_Game
+{
+	public class OptionBar : TriggerEntity
+	{
+		private int percentage;
+
+		public int Percentage {
+			get {
+				return percentage;
+			}
+			set {
+				percentage = value;
+			}
+		}
+
+		public OptionBar (string id, float x, float y, float width, float height, float rotation, 
+			BoundingBox hitbox, int layer, Texture2D texture, int percentage)
+			: base(id, x, y, width, height, rotation, hitbox, layer, texture)
+		{
+			this.percentage = percentage;
+		}
+	}
+}
+
--- Survival_Game/Menu/OptionMenu.cs
+++ Survival_Game/Menu/OptionMenu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using Game_Engine;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -12,6 +13,9 @@ namespace Survival_Game
 		private GameEngine engine;
 		private Button backBtn;
 		private RenderedEntity menu;
+		private OptionBar volume;
+		private CheckBox fullscreen;
+
 		public OptionMenu(GameEngine engine){
 			this.engine = engine;
 			float btnXPos = engine.GetScreenSize ().Width / 2;
@@ -21,14 +25,27 @@ namespace Survival_Game
 				new BoundingBox (new Vector3 (0, 0, 0), new Vector3 (0, 0, 0)), 1, null, false, false);
 
 			menu = new RenderedEntity ("menu", engine.GetScreenSize().Width / 2, engine.GetScreenSize().Height / 2, 600, 480, 0, 
-				new BoundingBox(), 0, null, false);		}
+				new BoundingBox(), 0, null, false);
+
+			volume = new OptionBar("Master Volume", engine.GetScreenSize().Width / 2, engine.GetScreenSize().Height / 2, 600, 480, 0, 
+				new BoundingBox(), 1, null, 10);
+
+			fullscreen = new CheckBox();
+			fullscreen.Appearance = Appearance.Button;
+		}
 
 		public void CreateMenu(){
-			
+			engine.ViewPositions.Add (new Tuple<Vector3, Viewport, Entity> (new Vector3 (0, 0, 0), engine.GraphicsDevice.Viewport, null));
+			engine.AddEntity(backBtn);
+			//engine.Add(fullscreen);
 		}
 
 		public void AddOptionButtonListener(){
 
+		}
+
+		public void AddBackButtonListener(Button.buttonPressed buttonListener){
+			backBtn.pressed += buttonListener;
 		}
 	}
 }
